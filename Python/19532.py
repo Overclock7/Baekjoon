2fd83b27cbc836fe2c86e4a99b29b0b4b1dab3b4
Overclock7 <ktj708@outlook.com>
2023-09-07 19:23:43 +0900

    2023-09-07

---
+++ Python/19532.py
@@ -0,0 +1,22 @@
+
+a,b,c,d,e,f = map(int,input().split())
+
+for x in range(-999,1000):
+    for y in range(-999,1000):
+        if a*x+b*y == c and d*x+e*y == f:
+            print(x,y)
+
+# import numpy as np
+
+# a,b,c,d,e,f = map(int,input().split())
+
+# Cremer's rule 사용
+
+# g = np.array([[c,b],[f,e]])
+# h = np.array([[a,c],[d,f]])
+# i = np.array([[a,b],[d,e]])
+
+# x = int(np.linalg.det(g) / np.linalg.det(i))
+# y = int(np.linalg.det(h) / np.linalg.det(i))
+
+# print(x, y)
