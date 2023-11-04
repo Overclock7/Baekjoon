1c69414b64dedab9ed512a1eb4f98b7988a18f4f
Overclock7 <ktj708@outlook.com>
2023-11-05 01:25:37 +0900

    주석 추가

--- Python/1013.py
+++ Python/1013.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 
 p = re.compile('(100+1+|01)+')
 for _ in range(int(input())):
-    if p.fullmatch(input().rstrip()):
+    if p.fullmatch(input().rstrip()): #전체 문자열 체크
         print('YES')
     else:
         print("NO")
